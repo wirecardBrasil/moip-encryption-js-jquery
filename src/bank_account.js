e65e4a6eb187b0cd16f08d9b95d492343e77739e
William Souza <willmiranda@gmail.com>
2015-10-28 14:11:13 -0200

    Removendo setters

--- src/bank_account.js
+++ src/bank_account.js
@@ -33,26 +33,6 @@
         return Moip.BancoDoBrasilValidator.agencyNumberIsValid(this);
         break;
     }
-  },
-
-  _setPubKey : function(key){
-    this.pubKey = key;
-  },
-  
-  _setNumber : function(number){
-    this.number = number;
-  },
-  
-  _setCvc : function(cvc){
-    this.cvc = cvc;
-  },
-  
-  _setExpMonth : function(expMonth){
-    this.expMonth = expMonth;
-  },
-  
-  _setExpYear : function(expYear){
-    this.expYear = expYear;
   }
   
  };
