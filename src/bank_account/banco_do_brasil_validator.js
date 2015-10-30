2c33439adb5703ef2c0157324e7b56890aa757f7
William Souza <willmiranda@gmail.com>
2015-10-30 14:11:48 -0200

    Refatoracao do bank_account.js e demo para bank_account

--- src/bank_account/banco_do_brasil_validator.js
+++ src/bank_account/banco_do_brasil_validator.js
@@ -1,24 +1,20 @@
 (function(window) {
-    var Moip = window.Moip || {};
-    window.Moip = Moip;
+  var Moip = window.Moip || {};
+  window.Moip = Moip;
 
-    function BancoDoBrasilValidator() {
-        if ( !( this instanceof BancoDoBrasilValidator ) ) {
-            return new BancoDoBrasilValidator();
-        }
+  function BancoDoBrasilValidator() {
+    if ( !( this instanceof BancoDoBrasilValidator ) ) {
+      return new BancoDoBrasilValidator();
     }
+  }
 
-    BancoDoBrasilValidator.prototype = {
+  BancoDoBrasilValidator.prototype = {
 
-        isValid : function(bankAccount) {
-            return this.agencyNumberIsValid(bankAccount.agencyNumber);
-        },
+    agencyNumberIsValid: function(agencyNumber) {
+      return agencyNumber.length == 4;
+    }
+  };
 
-        agencyNumberIsValid : function(agencyNumber) {
-            return agencyNumber.length == 4;
-        }
-    };
-
-    Moip.BancoDoBrasilValidator = BancoDoBrasilValidator();
+  Moip.BancoDoBrasilValidator = BancoDoBrasilValidator();
 
 })(window);
--- src/bank_account/bank_account.js
+++ src/bank_account/bank_account.js
@@ -1,44 +1,46 @@
 (function(window) {
 
- var Moip = window.Moip || {};
- window.Moip = Moip;
- 
- function BankAccount(params) {
+  var Moip = window.Moip || {};
+  window.Moip = Moip;
+
+  function BankAccount(params) {
     this.bankNumber = params.bankNumber;
     this.agencyNumber = params.agencyNumber;
     this.agencyCheckNumber = params.agencyCheckNumber;
     this.accountNumber = params.accountNumber;
     this.accountCheckNumber = params.accountCheckNumber;
- }
-
- BankAccount.prototype = {
-
-  isValid : function (){
-    if(!this.bankNumber)
-      return false;
-
-    switch(this.bankNumber){
-      case "001":
-        return Moip.BancoDoBrasilValidator.isValid(this);
-        break;
-      default:
-        return Moip.DefaultBankValidator.isValid(this);
-    }
-  },
-
-  agencyNumberIsValid : function (){
-    if(!this.bankNumber)
-      return false;
-    
-    switch(this.bankNumber){
-      case "001":
-        return Moip.BancoDoBrasilValidator.agencyNumberIsValid(this);
-        break;
-    }
   }
+
+  BankAccount.prototype = {
+
+    validate : function (callbacks){
+      if(!this.bankNumber)
+        return false;
+
+      var validator;
+
+      switch(this.bankNumber){
+        case "001":
+          validator = Moip.BancoDoBrasilValidator;
+          break;
+        default:
+          validator = Moip.DefaultBankValidator;
+      }
+
+      var errors = new Array();
+      if(!validator.agencyNumberIsValid(this.agencyNumber)){
+        errors.push({ description: "Agência inválida", code: "AGENCY_NUMBER" })
+      }
+
+      if(errors.length == 0) {
+        callbacks.valid();
+      } else {
+        callbacks.invalid({ errors: errors });
+      }
+    }
   
- };
- 
- Moip.BankAccount = BankAccount;
+  };
+
+  Moip.BankAccount = BankAccount;
 
 })(window);
