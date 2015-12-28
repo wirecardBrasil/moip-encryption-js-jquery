7283fab7d15327256477a52c47c5824e62d0c91e
William Souza <willmiranda@gmail.com>
2015-12-28 14:42:47 -0200

    Removendo trailing space

--- src/bank_account/banco_do_brasil_validator.js
+++ src/bank_account/banco_do_brasil_validator.js
@@ -29,7 +29,7 @@
       var checkNumberCalculated = Moip.BancoDoBrasilCheckNumberCalculator.calculateAgency(bankAccount.agencyNumber);
       return checkNumberCalculated === bankAccount.agencyCheckNumber;
     },
-    
+
     accountCheckNumberMatch: function(bankAccount) {
       var checkNumberCalculated = Moip.BancoDoBrasilCheckNumberCalculator.calculateAccount(bankAccount.accountNumber);
       return checkNumberCalculated === bankAccount.accountCheckNumber;
