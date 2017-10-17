1e8ba1dd79320ade277e9ea2d39b64a2013ce671
Lucas Queiroz <lucas.queiroz@moip.com.br>
2017-10-17 15:53:59 -0200

    Change length of account number

--- src/bank_account/banrisul_validator.js
+++ src/bank_account/banrisul_validator.js
@@ -47,7 +47,7 @@
       return Moip.CommonBankAccountValidator.accountNumberMsgError(this.accountNumberLength());
     },
 
-    accountNumberLength: function() { return 9; }
+    accountNumberLength: function() { return 8; }
     
   };
 
